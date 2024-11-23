cf8f102103bbc323728514f198e7cc7a6a08946b
azulws <39834637+azulws@users.noreply.github.com>
2024-11-23 16:29:10 -0300

    Revert "floor"
    This reverts commit e7fd382053da2415bba990ffe6c7682bfb4203ae.

--- gamejam/codigo/Stopwatch.gd
+++ gamejam/codigo/Stopwatch.gd
@@ -1,9 +1,8 @@
 extends Node
+class_name Stopwatch
 
-@onready var main: Node2D = $"../../.."
+@onready var main: Node2D = $".."
 
-
-var total_time=140
 var time= 140
 var stopped = true
 
@@ -18,14 +17,12 @@ func time_to_string() -> String:
 	var msec = fmod(time, 1) * 1000
 	var sec= fmod(time, 60)
 	var min= time/60
-	var format_string= "%02d : %02d"
-	var actual_string= format_string % [min, sec]
+	var format_string= "%02d : %02d : %02d"
+	var actual_string= format_string % [min, sec, msec]
 	return actual_string
 
-func time_to_float() -> float:
-	return (time/total_time)*100
 
 func _on_timer_timeout() -> void:
-	main.game_started= true
+	main.game_started=true
 	stopped = false
 	return
--- gamejam/codigo/timer.gd
+++ gamejam/codigo/timer.gd
@@ -1,14 +1,13 @@
 extends Control
 
 @export var StopwatchLabel: Label
-@onready var timer: Timer = $Timer
-@onready var stopwatch: Node = $Stopwatch
-@onready var rounded: TextureProgressBar = $Rounded
 
+var stopwatch : Stopwatch
 
+func _ready():
+	stopwatch = get_tree().get_first_node_in_group("stopwatch")
 	
 func _process(delta: float) -> void:
-	rounded.value=stopwatch.time_to_float()
 	_update_stopwatch_label()
 
 func _update_stopwatch_label():
